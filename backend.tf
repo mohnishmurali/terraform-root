a322013dabd04161e7827fb86020db1ff927c84a
Mohnish M <74337141+mohnishmurali@users.noreply.github.com>
2026-08-15 18:25:13 +0530

    Change S3 bucket name for Terraform backend

--- backend.tf
+++ backend.tf
@@ -9,7 +9,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "young-minds-app-terraform-state"
+    bucket         = "mohnish-project-2-bucket"
     region         = "ap-south-1"
     key            = "batch-32/terraform.tfstate"
     encrypt        = true
